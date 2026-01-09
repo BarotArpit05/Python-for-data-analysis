5fcb7c32e4f5bc326e64bf20c2b54f5cc3d4e0b7
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-09 13:56:41 +0530

    Updated list example and slicing

--- Chapter 4/01_list.py
+++ Chapter 4/01_list.py
@@ -2,4 +2,9 @@
 friends=["arpit","hello",544,67.44,False,"he"]
 print(friends)
 
+# List are Mutuable 
 print(friends[0])#here we can print element on index also 
+friends[0]="Grapes"
+print(friends)
+
+print(friends[1:4])#list Slicing

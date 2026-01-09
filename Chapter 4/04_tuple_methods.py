ca51f3fd32e2a775093fc57945c2a846262acfb4
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-09 14:25:10 +0530

    Methods of Tuple

---
+++ Chapter 4/04_tuple_methods.py
@@ -0,0 +1,6 @@
+#the various methods of tuple are
+d=("hello",23,"hii",23)
+no=d.count(23)#count method is to  see the element how many times commes in a tuple
+#print(no)
+i=d.index("hii")#index method finds where the element is in the index
+print(i)

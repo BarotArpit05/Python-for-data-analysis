63d9fed1b780cc4dd34f366e915e6bb1a69c83de
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-09 14:18:55 +0530

    Tuple Creation

---
+++ Chapter 4/03_tuple.py
@@ -0,0 +1,11 @@
+#Tuple is immutable in nature and is represent by () brackets
+a=(1,2,3,4,5)
+print(type(a))
+
+#empty tuple
+b=()
+#one element tuple
+c=(1,)
+#multiple element tuple
+d=("hello",23,"hii")
+print(d)

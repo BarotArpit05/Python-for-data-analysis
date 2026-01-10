29bb96ba0f500cc0e849bb24c2b2bf7bc6da9325
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-10 15:49:04 +0530

    Set Methods

---
+++ Chapter 5/04_set_method.py
@@ -0,0 +1,11 @@
+s={1,2,3,4,543,"hari"}
+print(s,type(s))
+s.add(345)#add element in set
+
+s.remove(1)#remove an element from set
+
+s.pop()#removes random element from set
+
+#s.clear()#clear the element of the  set
+
+print(s)

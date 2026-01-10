7746b9fb291c2023696cb60803eacb86e7a682fe
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-10 15:48:34 +0530

    Dictionary

---
+++ Chapter 5/01_dict.py
@@ -0,0 +1,10 @@
+marks={ #"key":"value"
+    "Arpit":100,
+    "Akash":24,
+    "Ajay":67,
+    "my_list":[10,"hari",34],
+    0:"hello"
+}
+d={}#empty dictionary
+print(marks,type(marks))
+print(marks["Arpit"])

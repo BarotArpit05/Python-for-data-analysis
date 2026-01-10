85fc50154007bfc3f9f62a50eaf489c0672cb988
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-10 15:49:17 +0530

    Set Union and Intersection

---
+++ Chapter 5/05_set_union_intersection.py
@@ -0,0 +1,9 @@
+s1={12,34,56,67,}
+s2={34,56,11,68}
+print(s1.union(s2))#give all values without repetition
+
+print(s1.intersection(s2))#give only common values in the both set
+
+print(s1.difference(s2))#will  do s1-s2 and the value remaining in the s1 will  be return 
+
+print(s2.difference(s1))#will  do s2-s1 and the value remaining in the s2 will  be return 

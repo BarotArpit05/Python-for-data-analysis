33da4dfd703ea0a7bfc4f93b5f82130669e9eecf
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-13 14:59:18 +0530

    Practice Set of Conditional Statement

---
+++ Chapter 6-PS/problem5.py
@@ -0,0 +1,8 @@
+#Write a program which finds out whether a given name is present in a list or not.
+my_list=["arpit","aksh","hari","ram"]
+
+name=input("Enter name :")
+if(name in my_list):
+    print("Name is in the list")
+else:
+    print("Name is not in the list")

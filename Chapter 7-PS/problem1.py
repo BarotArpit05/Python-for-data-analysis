e8810e3016532bfdea7fde94eabd500b2fff8bbd
BarotArpit05 <arpitbarot2006@gmail.com>
2026-01-16 14:57:40 +0530

    Practice Set

---
+++ Chapter 7-PS/problem1.py
@@ -0,0 +1,4 @@
+#Write a program to print multiplication table of a given number using for loop.
+a=int(input("Enter the number you want multiplicaation table of :"))
+for i in range(1,11):
+    print(f"{a} x {i} = {a*i}")
---
+++ Chapter 7-PS/problem3.py
@@ -0,0 +1,6 @@
+#Attempt problem 1 using while loop.
+a=int(input("Enter the number you want multiplicaation table of :"))
+i=1
+while(i<11):
+    print(f"{a} x {i} = {a*i}")
+    i+=1
